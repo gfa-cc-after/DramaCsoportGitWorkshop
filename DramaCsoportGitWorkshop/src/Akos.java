74828a4c9722ecedf299d78b248ed9a6ffb826ac
Seib182 <akos.szeibert@gmail.com>
2024-06-18 21:14:05 +0200

    c5

--- DramaCsoportGitWorkshop/src/Akos.java
+++ DramaCsoportGitWorkshop/src/Akos.java
@@ -1,5 +1,5 @@
 public class Akos {
     public static void cheer(String teamName, int teamSize) {
-        System.out.println(teamName + " has " + teamSize + " members.");
+        System.out.println(teamName + " has " + teamSize + " members!");
     }
 }
